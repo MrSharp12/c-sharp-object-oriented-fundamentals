89ec606f668a06228abd7dd3d645f01e312be582
Matt Sharp <matthewssharp1@gmail.com>
2018-05-29 13:49:12 +0000

    set up our objects

---
+++ ACM.BL/Order.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Order
+    {
+        public Order()
+        {
+
+        }
+
+        public Order(int OrderId)
+        {
+            this.OrderId = OrderId;
+        }
+
+        public DateTimeOffset? OrderDate { get; set; }
+        public int OrderId { get; private set; }
+
+        /// <summary>
+        /// Retrieve one order
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        public Order Retrieve(int orderId)
+        {
+            //code that retrieves the defined order
+            return new Order();
+        }
+
+        /// <summary>
+        /// Saves the current order
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            //code that saves a defined order
+            return true;
+        }
+
+        /// <summary>
+        /// Validates the current order
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderDate == null) isValid = false;
+
+            return isValid;
+        }
+    }
+}
